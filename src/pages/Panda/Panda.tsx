bdc9cd005dae89563a0c504897a826e6a47714c8
Oxana Danilova <oxanadanilova11@gmail.com>
2023-02-07 16:15:43 +0100

    feat: add Read more/Read less function

--- src/pages/Panda/Panda.tsx
+++ src/pages/Panda/Panda.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Sidebar from '../../components/Sidebar/Sidebar';
 import VideoSection from '../../components/VideoSection/VideoSection';
@@ -10,6 +10,14 @@ import Leaf from '../../components/Leaf/Leaf';
 import './Panda.scss';
 
 export default function Panda() {
+  const [shortText, setShortText] = useState(true);
+  const readMore = () => {
+    if (shortText) {
+      setShortText(false);
+    } else {
+      setShortText(true);
+    }
+  };
   const animalShortInfo = (
     <p>
       Giant pandas are very <span> unusual animals </span> that eat almost exclusively{' '}
@@ -41,7 +49,11 @@ export default function Panda() {
             shortInfo={animalShortInfo}
           />
           <div className="information-second-wrapper">
-            <div className="animal-information-block short-text">
+            <div
+              className={
+                shortText ? 'animal-information-block short-text' : 'animal-information-block'
+              }
+            >
               <div className="information-block">
                 <h5 className="information-subtitle">Diet:</h5>
                 <p className="information-description">
@@ -51,9 +63,7 @@ export default function Panda() {
               </div>
               <div className="information-block range">
                 <h5 className="information-subtitle">Range:</h5>
-                <p className="information-description">
-                  Eastern Asia <span>[VIEW MAP]</span>
-                </p>
+                <p className="information-description">Eastern Asia</p>
               </div>
               <div className="information-block habitat">
                 <h5 className="information-subtitle">Habitat:</h5>
@@ -74,10 +84,10 @@ export default function Panda() {
                 scent. Males regularly scent-mark the margins of their territories by adopting a
                 “headstand” position to mark a bamboo stalk with their anogenital glands.
               </p>
-              <h5 className="information-subtitle general">
+              <h5 className="information-subtitle general hide">
                 Physical features and characteristics:
               </h5>
-              <p className="information-description margin-top-ten">
+              <p className="information-description margin-top-ten hide">
                 The low nutrient value of bamboo figures prominently in the biology of the giant
                 panda. They have extremely powerful jaws and associated muscles to crunch through
                 the woody stems of bamboo. Each hand bears a{' '}
@@ -91,15 +101,15 @@ export default function Panda() {
                 that is constrained to a low-metabolism. This lifestyle explains why giant pandas
                 sleep so often and why they eat such <span>enormous</span> amounts of bamboo.
               </p>
-              <p className="information-description margin-top-twenty">
+              <p className="information-description margin-top-twenty hide">
                 Individuals do not travel long distances, and males will establish territories that
                 are not particularly large compared with other species in the Order Carnivora. Adult
                 females average about 230 pounds in weight, with males sometimes reaching 350
                 pounds. Their hair is very dense, conserving their body heat in their cold habitats.
                 Their iconic coloration remains a mystery to scientists but may aid with camouflage.
               </p>
-              <h5 className="information-subtitle general">Lifestyle and reproduction:</h5>
-              <p className="information-description margin-top-ten">
+              <h5 className="information-subtitle general hide">Lifestyle and reproduction:</h5>
+              <p className="information-description margin-top-ten hide">
                 Females are fertile for only a very short period of time and exhibit delayed
                 implantation. Although twins are not unusual, a mother will typically only rear one
                 cub. Adults are solitary, with mating pairings being quite brief. The tiny cubs are
@@ -108,26 +118,28 @@ export default function Panda() {
                 their own body temperatures. As a result, maternal care is extensive. Cubs do not
                 begin eating bamboo until they are around a year old.
               </p>
-              <p className="information-description margin-top-twenty">
+              <p className="information-description margin-top-twenty hide">
                 When twins are born in zoos, extraordinary measures are taken to train the female to
                 accept swapping of her cubs regularly 24 hours a day, so that she only cares for one
                 a time, but that both receive adequate care.
               </p>
-              <h5 className="information-subtitle general">Habitat:</h5>
-              <p className="information-description margin-top-ten">
+              <h5 className="information-subtitle general hide">Habitat:</h5>
+              <p className="information-description margin-top-ten hide">
                 Their preferred cool and wet bamboo forests are found at <span>elevations</span>{' '}
                 between 5,000 and 10,000 feet. Cubs are raised in rocky crevices, dead trees, or
                 caves.
               </p>
-              <h5 className="information-subtitle general">Diet:</h5>
-              <p className="information-description margin-top-ten">
+              <h5 className="information-subtitle general hide">Diet:</h5>
+              <p className="information-description margin-top-ten hide">
                 Our animal care professionals offer each adult or subadult giant panda about 80
                 pounds of various bamboo species per day, and they generally select and consume only
                 about third of that. They also are offered nutritious{' '}
                 <span>high-fiber biscuits</span> and fruits such as apples or sweet potatoes.
               </p>
             </div>
-            <button className="read-less">Read More</button>
+            <button className="read-less" onClick={() => readMore()}>
+              {shortText ? 'Read More' : 'Read Less'}
+            </button>
           </div>
         </div>
 
